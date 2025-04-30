81b9418d1bc9e6eb22a460426b3e1e9dd7ffb898
rdemora2 <robertomoraeszar@gmail.com>
2025-04-30 17:13:20 -0300

    REFACTOR/altera layout da home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,23 +5,23 @@ import Features from "components/features/ThreeColSimple.jsx";
 import MainFeature from "components/features/TwoColSingleFeatureWithStats.jsx";
 import SliderCard from "components/cards/ThreeColSlider.jsx";
 import TrendingCard from "components/cards/TwoTrendingPreviewCardsWithImage.jsx";
-import Blog from "components/blogs/PopularAndRecentBlogPosts.jsx";
 import Testimonial from "components/testimonials/TwoColumnWithImageAndProfilePictureReview.jsx";
 import FAQ from "components/faqs/SimpleWithSideImage.jsx";
-import SubscribeNewsLetterForm from "components/forms/SimpleSubscribeNewsletter.jsx";
 import Footer from "components/footers/MiniCenteredFooter.jsx";
+// import SubscribeNewsLetterForm from "components/forms/SimpleSubscribeNewsletter.jsx";
+// import Blog from "components/blogs/PopularAndRecentBlogPosts.jsx";
 
 export default () => (
   <AnimationRevealPage>
     <Hero />
+    <MainFeature />
     <Features />
     <SliderCard />
     <TrendingCard />
-    <MainFeature />
-    <Blog />
+    {/* <Blog /> */}
     <Testimonial textOnLeft={true} />
     <FAQ />
-    <SubscribeNewsLetterForm />
+    {/* <SubscribeNewsLetterForm /> */}
     <Footer />
   </AnimationRevealPage>
 );
