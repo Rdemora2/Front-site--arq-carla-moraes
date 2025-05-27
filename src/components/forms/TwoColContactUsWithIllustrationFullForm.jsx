0f93bc0742279f868b6f4d8bdfce3f00f2eba883
rdemora2 <robertomoraeszar@gmail.com>
2025-05-26 21:48:24 -0300

    STYLE/ remove campo "nosso endereço"

--- src/components/forms/TwoColContactUsWithIllustrationFullForm.jsx
+++ src/components/forms/TwoColContactUsWithIllustrationFullForm.jsx
@@ -243,18 +243,6 @@ const ContactForm = ({
             <InfoBlock>
               <IconContainer>
                 <svg viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M12,3L2,12h3v8h14v-8h3L12,3z M12,16c-1.1,0-2-0.9-2-2c0-1.1,0.9-2,2-2s2,0.9,2,2C14,15.1,13.1,16,12,16z" />
-                </svg>
-              </IconContainer>
-              <InfoText>
-                <InfoTitle>Nosso Endereço</InfoTitle>
-                <InfoValue>São Paulo, SP - Brasil</InfoValue>
-              </InfoText>
-            </InfoBlock>
-
-            <InfoBlock>
-              <IconContainer>
-                <svg viewBox="0 0 24 24" fill="currentColor">
                   <path d="M20.01 15.38c-1.23 0-2.42-.2-3.53-.56-.35-.12-.74-.03-1.01.24l-1.57 1.97c-2.83-1.35-5.48-3.9-6.89-6.83l1.95-1.66c.27-.28.35-.67.24-1.02-.37-1.11-.56-2.3-.56-3.53 0-.54-.45-.99-.99-.99H4.19C3.65 3 3 3.24 3 3.99 3 13.28 10.73 21 20.01 21c.71 0 .99-.63.99-1.18v-3.45c0-.54-.45-.99-.99-.99z" />
                 </svg>
               </IconContainer>
